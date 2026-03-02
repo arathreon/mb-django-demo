7b1aea7da9c93ff33d48e4850ff2f08e9b69c8c1
arathreon <arathreon@users.noreply.github.com>
2026-03-02 10:51:51 +0100

    Added normalized columns to models

--- csfdtop/utils.py
+++ csfdtop/utils.py
@@ -1,5 +1,15 @@
 import hashlib
+import unicodedata
 
 
 def get_name_year_hash(name, year):
+    """Hashes the name and year to create a unique identifier"""
     return hashlib.sha256(f"{name}{year}".encode()).hexdigest()
+
+
+def normalize_text(text):
+    """Removes diacritics and converts to lowercase"""
+    if not text:
+        return ""
+    nfkd_form = unicodedata.normalize("NFKD", text)
+    return "".join([c for c in nfkd_form if not unicodedata.combining(c)]).lower()
