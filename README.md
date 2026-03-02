85d452416a737e11067dc9eddbdb2d53b744a2b8
arathreon <arathreon@users.noreply.github.com>
2026-03-02 12:13:17 +0100

    Added view for searching films and actors.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Movies and actors
+# Films and actors
+
+## Setup
+The project is using `uv` as the package manager. To install the dependencies, run the following command
+in the terminal:
+
+```bash
+uv sync
+```
+
+## Usagge
+### Obtaining data
+To obtain the data, you need to run the command for getting data from the CSFD page. The command is invoked
+from the repository root via
+
+```bash
+python manage.py scrape
+```
+
+The data is stored in the SQLite database. Once the `scrape` command is run for the first time,
+every subsequent run will only add new data, not delete or update data for existing films and/or actors.
+
+### Running the application
+To run the application, you need to run the command for starting the server
+
+```bash
+python manage.py runserver 8000
+```
+
+This starts the *development* server on port `80000`.
+
+> [!NOTE]
+> The development server is not suitable for production use.
+
 
--- csfdtop/views.py
+++ csfdtop/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Film, Actor
+from .utils import normalize_text
 
-# Create your views here.
+
+def search_view(request):
+    query = request.GET.get("q", "")
+
+    actor_results = []
+    film_results = []
+
+    if query:
+        normalized_query = normalize_text(query)
+
+        # Search Actors
+        actors = Actor.objects.filter(name_normalized__contains=normalized_query)
+        for actor in actors:
+            actor_results.append({"name": actor.name, "id": actor.id})
+
+        # Search Films
+        films = Film.objects.filter(name_normalized__contains=normalized_query)
+        for film in films:
+            film_results.append({"name": film.name, "id": film.id, "year": film.year})
+
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
+        return JsonResponse({"actors": actor_results, "films": film_results})
+
+    return render(request, "csfdtop/index.html")
